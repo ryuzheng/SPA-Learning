dd27936cc8d463a35aede889fdf12befc770297c
郑泽鑫 <576160471@qq.com>
2016-03-04 22:37:34 +0800

    给聊天滑块添加点击事项

--- spa/js/spa.shell.js
+++ spa/js/spa.shell.js
@@ -6,28 +6,31 @@ spa.shell = (function() {
   var
     configMap = {
       main_html: String() + '<div class="spa-shell-head">' +
-        '      <div class="spa-shell-head-logo"></div>' +
-        '      <div class="spa-shell-dead-acct"></div>' +
-        '      <div class="spa-shell-head-search"></div>' +
-        '    </div>' +
-        '    <div class="spa-shell-main">' +
-        '      <div class="spa-shell-main-nav"></div>' +
-        '      <div class="spa-shell-main-content"></div>' +
-        '    </div>' +
-        '    <div class="spa-shell-foot"></div>' +
-        '    <div class="spa-shell-chat">' +
-        '      <div class="spa-shell-modal"></div>' +
-        '    </div>',
-      chat_extend_time: 1000,
+        '<div class="spa-shell-head-logo"></div>' +
+        '<div class="spa-shell-dead-acct"></div>' +
+        '<div class="spa-shell-head-search"></div>' +
+        '</div>' +
+        '<div class="spa-shell-main">' +
+        '<div class="spa-shell-main-nav"></div>' +
+        '<div class="spa-shell-main-content"></div>' +
+        '</div>' +
+        '<div class="spa-shell-foot"></div>' +
+        '<div class="spa-shell-chat">' +
+        '<div class="spa-shell-modal"></div>' +
+        '</div>',
+      chat_extend_time: 250,
       chat_retract_time: 300,
       chat_extend_height: 450,
-      chat_retract_height: 15
+      chat_retract_height: 15,
+      chat_extended_title: 'Click to retract',
+      chat_retracted_title: 'Click to extend'
     },
     stateMap = {
-      $container: null
+      $container: null,
+      is_chat_retracted: true
     },
     jqueryMap = {},
-    setJqueryMap, toggleChat, initModule;
+    setJqueryMap, toggleChat, onClickChat, initModule;
   //End 声明所有变量
 
   //Begin Utility Methods 保留区块，不与页面元素交互
@@ -55,6 +58,9 @@ spa.shell = (function() {
   //返回值：布尔型
   //  *true -滑块动画激活
   //  *false -滑块动画不激活
+  //状态：  设置stateMap.is_chat_retracted
+  //  *true -滑块是收起状态
+  //  *false -滑块是展开状态
   //
   toggleChat = function(do_extend, callback) {
     var
@@ -74,6 +80,10 @@ spa.shell = (function() {
           height: configMap.chat_extend_height
         }, configMap.chat_extend_time,
         function() {
+          jqueryMap.$chat.attr(
+            'title', configMap.chat_extended_title
+          );
+          stateMap.is_chat_retracted = false;
           if (callback) {
             callback(jqueryMap.$chat);
           }
@@ -86,6 +96,10 @@ spa.shell = (function() {
     jqueryMap.$chat.animate({
       height: configMap.chat_retract_height
     }, configMap.chat_retract_time, function() {
+      jqueryMap.$chat.attr(
+        'title', configMap.chat_retracted_title
+      );
+      stateMap.is_chat_retracted = true;
       if (callback) {
         callback(jqueryMap.$chat);
       }
@@ -97,6 +111,10 @@ spa.shell = (function() {
   //end 创建和操作DOM的函数
 
   //begin Event Handles jquery事件处理函数
+  onClickChat = function(event) {
+    toggleChat(stateMap.is_chat_retracted);
+    return false;
+  };
   //end Event Handles jquery事件处理函数
 
   //begin 公共方法
@@ -107,16 +125,22 @@ spa.shell = (function() {
     $container.html(configMap.main_html);
     setJqueryMap();
 
-    //test toggle
-    setTimeout(function() {
-      toggleChat(true);
-    }, 3000);
-    setTimeout(function() {
-      toggleChat(false);
-    }, 8000);
+    // initialize chat slider and bind click handler 设置 stateMap.is_chat_retracted 的值和光标悬停文字，初始化事件处理程序
+    stateMap.is_chat_retracted = true;
+    jqueryMap.$chat
+      .attr('title', configMap.chat_retracted_title)
+      .click(onClickChat);
+
+    // //test toggle
+    // setTimeout(function() {
+    //   toggleChat(true);
+    // }, 3000);
+    // setTimeout(function() {
+    //   toggleChat(false);
+    // }, 8000);
   };
   //end public method /initModule/
-  
+
   return {
     initModule: initModule
   };
